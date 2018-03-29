3b4e5814e44ed2d835048c4f06202f6692fa559b
farook <farooksoft@gmail.com>
2018-03-29 16:57:27 +0530

    ajax

---
+++ Demo1.js
@@ -0,0 +1,13 @@
+/*var a = "hello";
+var b = "world";
+var c = a + b;
+*/console.log("Hello world");
+/*console.log("Sum of 2 nos is:",c);*/
+console.log(sum())
+
+var sum = function(){
+	var a = "hello";
+	var b = "world";
+	var c = a + b;	
+	//console.log("Sum of 2 nos is:",c);
+}
---
+++ hello.js
@@ -0,0 +1,16 @@
+console.log("hello world");
+console.log('hello world');
+
+var x, y, z;  // Declare 3 variables
+x = 5;        // Assign the value 5 to x
+y = 6;        // Assign the value 6 to y
+z = x + y;    // Assign the sum of x and y to z
+
+console.log(z);
+
+var firstName = "Tom";
+var lastName = "Jerry";
+console.log(firstName+ " "+ lastName);
+
+var name = prompt("Enter your name:","");
+document.write(name);
